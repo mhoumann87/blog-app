3b74eb041ecb04cf60a4572de91e128602ad1471
Michael Houmann <mhoumann87@gmail.com>
2023-04-23 14:40:47 +0200

    Add page to shoe snippets of the posts
    The Feed.jsx component will display the snippets of the posts, that are coming from the Post.jsx.
    And it will all be displayed on the Home.jsx page

---
+++ src/components/Post.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Post = ({ post }) => {
+  return (
+    <article key={post.id} className='post'>
+      <Link to={`/post/${post.id}`}>
+        <h3>{post.title}</h3>
+        <p className='small'>Post date: {post.datetime}</p>
+      </Link>
+
+      <p>
+        {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)} ...`}
+      </p>
+    </article>
+  );
+};
+export default Post;
