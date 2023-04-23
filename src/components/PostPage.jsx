c1cac941d2472d59e07b0349581fcc6f888ba215
Michael Houmann <mhoumann87@gmail.com>
2023-04-23 14:39:58 +0200

    Add functionality to show a single post

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,7 +1,32 @@
-const PostPage = () => {
+import { useParams, Link } from 'react-router-dom';
+
+const PostPage = ({ posts, handelDelete }) => {
+  const { id } = useParams();
+
+  const post = posts.find(post => post.id.toString() === id);
+
   return (
-    <main>
-      <h2>All Our Posts</h2>
+    <main className='single-post'>
+      <article className='post'>
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p className='small'>Posted at: {post.datetime}</p>
+            <p>{post.body}</p>
+            <button onClick={() => handelDelete(post.id)}>Delete Post</button>
+          </>
+        )}
+        {!post && (
+          <>
+            <h3>The post in nowhere to find</h3>
+            <p>
+              That is truly embarrassing, and I will go to my corner and think
+              about what I have done{' '}
+            </p>
+            <Link to={'/'}>Instead you can visit our frontpage</Link>
+          </>
+        )}
+      </article>
     </main>
   );
 };
