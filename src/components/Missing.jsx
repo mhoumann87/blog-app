e8d5d22116a956431c003984ee6892c665fe3f12
Michael Houmann <mhoumann87@gmail.com>
2023-04-24 13:33:22 +0200

    Add image, style and content for Missing component

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+import img404 from '../images/img404.png';
+
 const Missing = () => {
   return (
-    <main>
-      <h2>404</h2>
+    <main className='missing-page'>
+      <img src={img404} alt="We couldn't find the page you were looking for." />
+      <Link to={'/'}>Back to the Frontpage</Link>
     </main>
   );
 };
