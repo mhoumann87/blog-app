39ec0d14dadf3410c3550f76adbbbb5a62541690
Michael Houmann <mhoumann87@gmail.com>
2023-04-24 11:47:04 +0200

    Add CSS and classes to PostPage component

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -6,14 +6,20 @@ const PostPage = ({ posts, handelDelete }) => {
   const post = posts.find(post => post.id.toString() === id);
 
   return (
-    <main className='single-post'>
-      <article className='post'>
+    <main className='single-post shadow'>
+      <article>
         {post && (
           <>
-            <h2>{post.title}</h2>
-            <p className='small'>Posted at: {post.datetime}</p>
-            <p>{post.body}</p>
-            <button onClick={() => handelDelete(post.id)}>Delete Post</button>
+            <div className='header-box flex'>
+              <h2>{post.title}</h2>
+              <p className='small'>Posted at: {post.datetime}</p>
+            </div>
+            <div className='post-body'>
+              <p>{post.body}</p>
+              <button className='shadow' onClick={() => handelDelete(post.id)}>
+                Delete Post
+              </button>
+            </div>
           </>
         )}
         {!post && (
