44b813a84682f11de145b79fd2b78a06e09dcbde
Michael Houmann <mhoumann87@gmail.com>
2023-04-24 10:05:14 +0200

    Update Nav.jsx and styles for mobile screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,30 +4,34 @@ const Nav = ({ search, setSearch }) => {
   return (
     <nav>
       <div className='nav-box flex'>
-        <ul className='flex'>
-          <li>
-            <Link to={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link to={'/post'}>New Post</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>About</Link>
-          </li>
-        </ul>
-        <form className='search-form' onAbort={e => e.preventDefault()}>
-          <label htmlFor='search' className='sr-only'>
-            search posts
-          </label>
-          <input
-            type='text'
-            id='search'
-            name='search'
-            placeholder='Search Posts'
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-          />
-        </form>
+        <div>
+          <ul className='nav-list flex'>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/post'}>New Post</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>About</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <form className='search-form' onAbort={e => e.preventDefault()}>
+            <label htmlFor='search' className='sr-only'>
+              search posts
+            </label>
+            <input
+              type='text'
+              id='search'
+              name='search'
+              placeholder='Search Posts'
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+            />
+          </form>
+        </div>
       </div>
     </nav>
   );
