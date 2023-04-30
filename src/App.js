26064873d74622c92e4c6d38f7a1e20540df9598
Michael Houmann <mhoumann87@gmail.com>
2023-04-30 11:25:18 +0200

    Update app to use easy-peasy

--- src/App.js
+++ src/App.js
@@ -6,29 +6,41 @@ import PostPage from './components/PostPage';
 import EditPost from './components/EditPost';
 import About from './components/About';
 import Missing from './components/Missing';
-import { DataProvider } from './context/DataContext';
 
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import { useStoreActions } from 'easy-peasy';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const setPosts = useStoreActions(actions => actions.setPosts);
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    'http://localhost:3500/posts'
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
   return (
     <>
       <Header title={'Blog APP'} />
-      <DataProvider>
-        <Routes>
-          <Route path='/' element={<Home />} />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home isLoading={isLoading} fetchError={fetchError} />}
+        />
 
-          <Route path='/post' element={<NewPost />} />
+        <Route path='/post' element={<NewPost />} />
 
-          <Route path={'/edit/:id'} element={<EditPost />} />
+        <Route path={'/edit/:id'} element={<EditPost />} />
 
-          <Route path='/post/:id' element={<PostPage />} />
+        <Route path='/post/:id' element={<PostPage />} />
 
-          <Route path='/about' element={<About />} />
+        <Route path='/about' element={<About />} />
 
-          <Route path='*' element={<Missing />} />
-        </Routes>
-      </DataProvider>
+        <Route path='*' element={<Missing />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,41 +1,30 @@
-import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
-import api from '../api/posts';
-import DataContext from '../context/DataContext';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 
 const NewPost = () => {
-  const { posts, setPosts } = useContext(DataContext);
-
   const navigate = useNavigate();
 
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
+  const posts = useStoreState(state => state.posts);
+  const postTitle = useStoreState(state => state.postTitle);
+  const postBody = useStoreState(state => state.postBody);
+
+  const savePost = useStoreActions(actions => actions.savePost);
+  const setPostTitle = useStoreActions(actions => actions.setPostTitle);
+  const setPostBody = useStoreActions(actions => actions.setPostBody);
 
   const handleSubmit = async e => {
     e.preventDefault();
-
     // Set and id based on the previous posts
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     // Find datetime
     const datetime = format(new Date(), 'MMMM dd yyyy pp');
     // create the new post
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    // Update the database
-    try {
-      const response = await api.post('/posts', newPost);
-      // create a new array with all posts
-      const allPosts = [...posts, response.data];
-      // update the post array
-      setPosts(allPosts);
-      // Clear the fields in the form
-      setPostTitle('');
-      setPostBody('');
-      // Go back to the frontpage
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: ${err}`);
-    }
+    // Save the post to the database
+    savePost(newPost);
+    // Send user back to the front page
+    navigate('/');
   };
 
   return (
